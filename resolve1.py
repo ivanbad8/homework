b463d1c08fb7ab386a0706a88188c19b0990c14a
ivanbad8 <badlaev8@gmail.com>
2024-06-25 22:58:12 +0900

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,17 @@
+first = 8
+second = 5
+third = 4
+summa = first + second
+diff = first - second
+mean = (first + second + third)/3
+first_sring = 'Вторник'
+second_string = 'Понедельник'
+a = 2
+b = 3
+c = 4
+f = 14
+print ('Длинна слова', len('добрый вечер'))
+print ('сумма разность',summa, diff)
+print ('среднее арифметическое',mean)
+print (second_string,',', first_sring) # простык строчки
+print (f*f*f//2)
