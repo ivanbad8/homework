72aed716b019b6dd7e6becb9192d9c6b0d2580af
ivanbad8 <badlaev8@gmail.com>
2024-06-29 22:45:58 +0900

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'ivan': 88002000600 , 'alex' : 890562654165}
+print(my_dict)
+my_dict.update({'valeri': 5646542514,'denis ' : 5231545646})
+print(my_dict)
+del my_dict['alex']
+print(my_dict)
+print(my_dict.get('alex'))
+my_set = [11, 12, 13, 1,2,3,]
+print(my_set)
+print(set(my_set))
+del my_set[1]
+print(my_set)
+
+
