88da6a17b7ddfa6641a15f7c61b4bb8578e00a60
ivanbad8 <badlaev8@gmail.com>
2024-06-29 21:33:20 +0900

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,6 +1,9 @@
-immutable_var = 2,4, 'home'
-immutable_var = (2,4)
-mutable_list  = 1,3, 'work'
-mutable_list = 1+3, 'work'
-print(immutable_var*3, immutable_var,mutable_list[-1])
+immutable_var = ([2,4], 'home')
+immutable_var[0][1] = 10
+immutable_var[0][0] = 5
+mutable_list  = [1,3, 'work']
+mutable_list[2] = 'free'
+print(immutable_var)
+print(mutable_list)
+
 
