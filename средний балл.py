afff55396789a8b4c0c170d1b94d72daf67cd4d8
ivanbad8 <badlaev8@gmail.com>
2024-07-05 13:10:55 +0900

    Add files via upload

---
+++ средний балл.py
@@ -0,0 +1,7 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = {'Johnyy','Bilbo','Steve','Khendrik','Aaron'}
+print({(sorted(students)[0]):((sum(grades[0])/len(grades[0]))),(sorted(students)[1]):((sum(grades[1])/len(grades[1]))),(sorted(students)[2]):((sum(grades[2])/len(grades[2]))),(sorted(students)[3]):((sum(grades[3])/len(grades[3]))),(sorted(students)[4]):((sum(grades[4])/len(grades[4])))})
+
+
+#(sum(grades[0])//len(grades[0]))
+
