5e3eeac6a8ba4b52e2d0a03f2cad77cdcafc9388
ivanbad8 <badlaev8@gmail.com>
2024-09-30 15:04:22 +0900

    Add files via upload

---
+++ module_8_3.py
@@ -0,0 +1,9 @@
+class Car:
+    __numbers =
+    def __init__(self, model: str, __vin: int):
+        self.model = model
+        self.__vin = __vin
+
+    def __is_valid_vin(vin_number):
+
+    def __is_valid_numbers(numbers):
