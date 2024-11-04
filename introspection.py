f20e35373750dc240d57493e49d70861569cdc74
ivanbad8 <badlaev8@gmail.com>
2024-11-04 15:04:27 +0900

    Add files via upload

---
+++ introspection.py
@@ -0,0 +1,37 @@
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+
+    attributes = []
+    for attr in dir(obj):
+        if not callable(getattr(obj, attr)) and not attr.startswith("__"):
+            attributes.append(attr)
+
+    methods = []
+    for method in dir(obj):
+        if callable(getattr(obj, method)) and not method.startswith("__"):
+            methods.append(method)
+
+    obj_module = obj.__module__
+
+    result = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': obj_module}
+
+    return result
+
+
+class SomeClass:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def amount(self):
+        return self.num1 + self.num2
+
+
+my_obj = SomeClass(10, 15)
+
+obj_info = introspection_info(my_obj)
+print(obj_info)
